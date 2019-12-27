49529dfab11cfacfb5256c5331b8197425f5a198
Andrew Basner <andrewbasner@gmail.com>
2019-12-27 11:17:28 -0600

    Add materialize css and placement navbar

--- app/javascript/Navbar.vue
+++ app/javascript/Navbar.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id='navbar'>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav>
+    <div class="nav-wrapper blue lighten-1" id='navbar'>
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><router-link to="/">Home</router-link>
+        <li><router-link to="/about">About</router-link></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 
@@ -12,5 +18,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
